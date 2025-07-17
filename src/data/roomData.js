d0f65b8cc52df84ba427e1be2c956cbb853c4df3
Ajayi Gloria <Adebayoadedoyin076@gmail.com>
2025-07-16 21:03:46 -1100

    added room  prices

--- src/data/roomData.js
+++ src/data/roomData.js
@@ -8,37 +8,50 @@ const roomData = [
     {
         id: 2,
         name: 'Gold',
-        price: '#20,000',
+        roomPrice: '#20,000',
+        shortRestPrice: '#4,000'
     },
       {
         id: 3,
         name: 'Favour',
-        price: '#20,000',
+        roomPrice: '#20,000',
+        shortRestPrice: '#4,000'
     },
     {
         id: 4,
         name: 'Grace',
-        price: '#18,000',
+       roomPrice: '#18,000',
+        shortRestPrice: '#4,000'
     },
      {
         id: 5,
         name: 'Peace',
-        price: '#18,000',
+        roomPrice: '#18,000',
+        shortRestPrice: '#4,000'
     },
       {
         id: 6,
         name: 'Vegas',
-        price: '#18,000',
+roomPrice: '#18,000',
+        shortRestPrice: '#4,000'
     },
     {
         id: 7,
         name: 'Diamond',
-        price: '#10,000',
+        roomPrice: '#10,000',
+        shortRestPrice: '#4,000'
     },
      {
         id: 8,
         name: 'Comfort',
-        price: '#10,000',
+        roomPrice: '#10,000',
+        shortRestPrice: '#4,000'
+    },
+     {
+        id: 9,
+        name: 'Love',
+        roomPrice: '#10,000',
+        shortRestPrice: '#4,000'
     },
     ];
 
